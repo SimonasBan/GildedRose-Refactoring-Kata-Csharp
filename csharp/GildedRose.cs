4cc468713c50d353d9822a1502b2e5c848d0f3e4
SimonasBan <sbansevicius@gmail.com>
2021-05-17 16:29:35 +0300

    Last minor cosmetic changes

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -31,6 +31,11 @@ namespace csharp
         }
 
         /*To read for evaluator.
+        * --------
+        * My solution is based on switch since
+        * the task asked to make the code readable
+        * and maintanable. This would make it easier to add new
+        * items in the future.
         * ---------- 
         * If I could change Item class, I would add
         * itemType property to it and make switch based on itemType.
@@ -40,19 +45,6 @@ namespace csharp
         * does not allow
         * me to do that as well since I wouldn't use
         * Items property anymore.
-        * ------
-        * My solution is based on switch since
-        * the task asked to make the code readable
-        * and maintanable. This would make it easier to add new
-        * items in the future.
-        * --------
-        * There was also an idea to
-        ad if (item.SellIn > 0) here and have
-        different switches for items that are old
-        and not since almost all of the items
-        act differently with age., nm
-        But this would decrease readability
-        as well as maintainability
         */
         public void UpdateQuality()
         {
@@ -95,9 +87,6 @@ namespace csharp
                 }
             }
         }
-
-
-
         public void HandleConcertChanges(Item item)
         {
             //SellIn < 0
@@ -182,121 +171,5 @@ namespace csharp
             }
             item.SellIn -= 1;
         }
-
-
-
-        //////Old solution
-        //public void UpdateQuality()
-        //{
-
-
-        //    foreach (var item in Items)
-        //    {
-        //        /*WHOLE IF - takes care of Quality
-        //         * -------                 * 
-        //         * Items: basic, Sulfurus*/
-        //        if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-        //        {
-        //            if (item.Quality > 0)
-        //            {
-        //                //If basic item
-        //                if (item.Name != "Sulfuras, Hand of Ragnaros")
-        //                {
-        //                    item.Quality = item.Quality - 1;
-        //                }
-        //            }
-        //            /*else{}
-        //             Cover edge case of item with quality<0*/
-        //        }
-        //        /*Items: Brie, Concert*/
-        //        else
-        //        {
-        //            if (item.Quality < 50)
-        //            {
-        //                //Always increases Brie and concert at least by 1
-        //                item.Quality = item.Quality + 1;
-
-        //                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-        //                {
-
-        //                    //< 11 is +2 (but 1 is already plussed by default)
-        //                    if (item.SellIn < 11)
-        //                    {
-        //                        //This is already checked I thik
-        //                        if (item.Quality < 50)
-        //                        {
-        //                            item.Quality = item.Quality + 1;
-        //                        }
-        //                    }
-
-        //                    /*< 6 is +3 (but 1 is already plussed
-        //                     * by default and another 1 because it was < 11)
-        //                     * --------
-        //                     * But this should be in an else of <11
-        //                     */
-        //                    if (item.SellIn < 6)
-        //                    {
-        //                        //This is already checked I thik
-        //                        if (item.Quality < 50)
-        //                        {
-        //                            item.Quality = item.Quality + 1;
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //            /*else{}
-        //             Cover edge case of item with quality>50*/
-        //        }
-
-        //        //Every item get older except Sulfuras.
-        //        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        //        {
-        //            item.SellIn = item.SellIn - 1;
-        //        }
-
-
-        //        /*WHOLE IF - takes care of age
-        //         * (decreases quality on edge cases caused by age)
-        //         Maybe could be put as an else*/
-        //        if (item.SellIn < 0)
-        //        {
-        //            //Basic, concert, sulfurues items
-        //            if (item.Name != "Aged Brie")
-        //            {
-        //                //Basic, sulfurus items
-        //                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-        //                {
-        //                    //This should be put earlier and not repeated
-        //                    if (item.Quality > 0)
-        //                    {
-        //                        /*Basic items.
-        //                        Check it earlier together with concert?*/
-        //                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        //                        {
-        //                            /*Basic items were already decreased
-        //                             * but with age <0 their Quality decreases by 2
-        //                            */
-        //                            item.Quality = item.Quality - 1;
-        //                        }
-        //                    }
-        //                }
-        //                //Concert items. Quality is zero when age is <0
-        //                else
-        //                {
-        //                    //Change to = 0
-        //                    item.Quality = item.Quality - item.Quality;
-        //                }
-        //            }
-        //            //Brie items. Get better no matter the age
-        //            else
-        //            {
-        //                if (item.Quality < 50)
-        //                {
-        //                    item.Quality = item.Quality + 1;
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
     }
 }
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -7,8 +7,6 @@ namespace csharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("OMGHAI!");
-
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -33,8 +31,7 @@ namespace csharp
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 15}
             };
 
             var app = new GildedRose(Items);
